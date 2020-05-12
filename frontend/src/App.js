540f761194815be6fabe5d9e90c1a1b67cb31790
Rafael Ibraim <ibraim.gm@gmail.com>
2020-05-12 12:27:09 -0300

    FE: Created basic RadioGroup component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
 import Header from './Header';
+import RadioGroup from './RadioGroup';
+
+const Frame = styled.div`
+  background: #f1f6fb 0% 0% no-repeat padding-box;
+  height: 100%;
+  text-align: center;
+`;
+
+const PayText = styled.p`
+  text-align: center;
+  font: Regular 14px/26px Montserrat;
+  letter-spacing: 0px;
+  color: #1d5297;
+  margin: 0;
+  padding: 50px 0 10px 0;
+`;
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Header />
-    </div>
+      <Frame>
+        <PayText>Quero pagar a cada:</PayText>
+        <RadioGroup
+          options={['3 anos', '1 ano', '1 mês']}
+          selected={'3 anos'}
+        />
+      </Frame>
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/RadioGroup.js
@@ -0,0 +1,74 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+import svgRadioOn from './images/radio-on.svg';
+import svgRadioOff from './images/radio-off.svg';
+
+const radioCss = () => css`
+  font: Regular 16px/23px Montserrat;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 2px 4px #4480c570;
+  border: 1px solid #4480c5;
+  border-radius: 21px;
+
+  display: inline-block;
+  list-style-type: none;
+  padding: 0;
+  margin: 6px auto;
+
+
+  li {
+    display: inline-block;
+    padding: 10px 16px;
+
+    span {
+      border: 0;
+      background: url('${svgRadioOff}') no-repeat padding-box;
+      width: 18px;
+      height: 18px;
+      padding-right: 20px;
+      padding-left: 5px;
+    }
+  }
+
+  .selected {
+    font: Bold 16px/23px Montserrat;
+    letter-spacing: 0px;
+    color: #FFFFFF;
+    background: #4480c5 0% 0% no-repeat padding-box;
+    border-radius: 21px;
+
+    span {
+      background: url('${svgRadioOn}') no-repeat padding-box;
+    }
+  }
+
+  li:first-child {
+    position: relative;
+    left: -2px;
+  }
+
+  li:last-child {
+    position: relative;
+    left: 2px;
+  }
+`;
+
+const RadioGroup = ({ options, selected }) => {
+  return (
+    <ul css={radioCss}>
+      {options.map((opt, idx) => {
+        const clsName = opt === selected ? 'selected' : '';
+
+        return (
+          <li key={idx} className={clsName}>
+            <span />
+            {opt}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default RadioGroup;
