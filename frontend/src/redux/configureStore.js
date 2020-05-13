271b1c07f3688615ecfa3e1cf1fb7a861ab3ac3f
Rafael Ibraim <ibraim.gm@gmail.com>
2020-05-13 14:50:59 -0300

    FE: Added support for Redux-DevTools

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import product from './modules/product';
 
 const rootReducer = combineReducers({ product });
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default () => createStore(rootReducer, applyMiddleware(thunk));
+export default () =>
+  createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
